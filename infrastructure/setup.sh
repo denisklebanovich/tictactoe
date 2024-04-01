7d3c10d18898df00200f62600dd7956c9cf2d2ca
John Silver <cr1me7lavi@tutanota.com>
2024-04-01 15:55:59 +0200

    fix

--- infrastructure/setup.sh
+++ infrastructure/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Install Docker and Docker Compose
+echo "Installing Docker and Docker Compose"
 sudo yum update -y
 sudo yum install docker -y
 sudo service docker start
@@ -7,9 +8,12 @@ sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Install Git
+echo "Installing Git"
 sudo yum install git -y
 
 # Clone the repository
+echo "Cloning the repository"
 git clone https://github.com/denisklebanovich/tictactoe.git
 cd tictactoe || exit
+echo "Starting the application"
 sudo docker-compose up -d
