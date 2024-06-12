b6556a3e7b0cec7ea59454d86de5d385cb960342
gguser <denis.klebanovich@gmail.com>
2024-06-12 14:08:44 +0200

    Fix

--- infrastructure/modules/storage/main.tf
+++ infrastructure/modules/storage/main.tf
@@ -45,7 +45,7 @@ resource "aws_security_group" "allow_backend_access" {
     from_port = 5432
     to_port   = 5432
     protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [var.security_group_id]
   }
 
   egress {
